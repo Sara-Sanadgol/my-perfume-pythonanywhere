6cb6c4213e7a021589ba8fffa25a615687867e71
Sara <sara_sanadgol@yahoo.com>
2018-07-31 18:46:48 -0400

    an empty commit

--- mywebsite/settings.py
+++ mywebsite/settings.py
@@ -14,25 +14,24 @@ import os
 import dj_database_url
 
 
-DEBUG = False
+# DEBUG = False
+#
+# ALLOWED_HOSTS = ['127.0.0.1', '.herokuapp.com']
+#
+#
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
+#         'NAME': 'djangoproject',
+#         'USER': 'name',
+#         'PASSWORD': '',
+#         'HOST': 'localhost',
+#         'PORT': '',
+#     }
+# }
 
-ALLOWED_HOSTS = ['127.0.0.1', '.herokuapp.com']
-
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'djangoproject',
-        'USER': 'name',
-        'PASSWORD': '',
-        'HOST': 'localhost',
-        'PORT': '',
-    }
-}
-
-
-db_from_env = dj_database_url.config(conn_max_age=500)
-DATABASES['default'].update(db_from_env)
+# db_from_env = dj_database_url.config(conn_max_age=500)
+# DATABASES['default'].update(db_from_env)
 
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
@@ -48,17 +47,16 @@ SECRET_KEY = '5*^*zmbs-90a=n#pudco#$4w2kaiu+tzw!z__*@li9j73qmitt'
 # SECURITY WARNING: don't run with debug turned on in production!
 
 
-#
-# DEBUG = True
-#
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-#     }
-# }
-#
-# ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com']
+DEBUG = True
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
+
+ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com']
 
 
 # Application definition
